d1c1350197dd1fc8439c2bf66d69e4788085aaf7
Alena Kuntsevich <Alena_Kuntsevich@epam.com>
2022-06-05 17:39:33 +0300

    feature: cli task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,10 @@
 export const parseArgs = () => {
-    // Write your code here 
+    let aArgs = process.argv.slice(2);
+    let result = '';
+    for (let i = 0; i < aArgs.length; i += 2) {
+        result += aArgs[i].slice(2) + ' is ' + aArgs[i + 1] + ', ';
+    }
+    result = result.slice(0, result.length - 2)
+    console.log(result)
 };
+parseArgs()
